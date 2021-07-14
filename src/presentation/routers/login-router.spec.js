b3b9ccdc3fadbe84d7cc61fa069c22790695fc71
Edmilton Pansanato <epansanato@mtsolucoes.com.br>
2021-07-14 00:12:56 -0300

    Add second test and husky width jest

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -11,15 +11,23 @@ class LoginRouter {
 describe('Login Router', () => {
   test('Should return 400 if no email is provided', () => {
     const sut = new LoginRouter()
-
     const httpRequest = {
       body: {
         password: '123456'
       }
     }
-
     const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 
+  test('should return 400 if no password is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: '123456@.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
 })
